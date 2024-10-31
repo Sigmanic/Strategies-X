44d1064c4db9849ec34516db44ec99ce93f43bb4
Gurty <synergysypher@gmail.com>
2024-10-31 07:03:33 +0700

    Added support for night 3

--- TDS/Functions/Map.lua
+++ TDS/Functions/Map.lua
@@ -17,6 +17,7 @@ local SpecialGameMode = {
     ["Polluted Wastelands II"] = {mode = "polluted", challenge = "PollutedWasteland"},
     ["Failed Gateway"] = {mode = "halloween2024", difficulty = "Act1", night = 1},
     ["The Nightmare Realm"] = {mode = "halloween2024", difficulty = "Act2", night = 2},
+    ["Containment"] = {mode = "halloween2024", difficulty = "Act3", night = 3},
 }
 local ElevatorSettings = {
     ["Survival"] = {Enabled = false, ReMap = true, JoinMap = true, WaitTimeRe = .1, WaitTimeJoin = .25},
