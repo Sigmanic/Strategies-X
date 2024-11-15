118b56fe4eb7358c89124e5164c77f3b6e812bf6
SkremGah <antostojcevic7@gmail.com>
2024-11-15 18:04:55 +0100

    Update MainSource.lua

--- MainSource.lua
+++ MainSource.lua
@@ -13,7 +13,7 @@ local Items = {
 }
 
 local LoadLocal = false
-local MainLink = LoadLocal and "" or "https://raw.githubusercontent.com/Sigmanic/Strategies-X/main/"
+local MainLink = LoadLocal and "" or "https://raw.githubusercontent.com/SkremGah/Strategies-X/main/"
 
 local OldTime = os.clock()
 
